3ccaa37947da04463030ae2a2073e1e625c11d5c
SimonHolmquist <simon.holmquist@gmail.com>
2020-04-20 10:26:12 -0300

    commit tp1_1.cpp

---
+++ tp1_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int *p, a = 5;
+    p = &a;
+    //a)
+    cout<<*p<<'\n';
+    //b)
+    cout<<p<<'\n';
+    //c)
+    cout<<&a<<'\n';
+    //d)
+    cout<<&p<<'\n';
+    //e)
+    cout<<sizeof(p)<<' '<<sizeof(a)<<'\n';    
+    return 0;
+}
